6dc2a56cd18b187ba173789cd86ded5aacdda70a
Mo Silvera <mo.leslie.silvera@gmail.com>
2020-01-27 10:15:00 -0600

    logout button working

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Nav.css'
+import authHandling from '../../hooks/authHandling'
+
+
 export default (props) => {
+
+    const { logout } = authHandling()
 
     return (
         <nav className="navbar">
@@ -15,7 +20,22 @@ export default (props) => {
 
                 <Link className="nav-link" to="/Employees">Employees</Link>
 
-                <Link className="nav-link" to="/login">Logout</Link>
+                {/* <Link className="nav-link" to="/login">Logout</Link> */}
+
+                {
+                    sessionStorage.getItem("credentials") !== null || localStorage.getItem("credentials") !== null
+                         ? <li className="navbar__item">
+                         <Link className="navbar__link"
+                         to=""
+                         onClick={e => {
+                         e.preventDefault()
+                        logout()
+                        props.history.push("/")
+                        }}
+                        >Logout</Link>
+                         </li>
+                         : ""
+                        }
         </nav>
     )
 }
